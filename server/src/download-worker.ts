76ea36598d03bcefce578f917b37359257f48225
Daniel Payet <danieldu974@gmail.com>
2022-08-13 09:39:22 +0200

    handle dns failure

--- server/src/download-worker.ts
+++ server/src/download-worker.ts
@@ -1,6 +1,13 @@
 import { DownloadData } from "./services/download-data.ts";
 
-self.onmessage = async () => {
-  self.postMessage(await new DownloadData().download())
+self.onmessage = async () => await downloadData();
+
+const downloadData = async (retry = 0) => {
+  const data = await new DownloadData().download();
+  if(data === undefined && retry <= 3) {
+    console.log("Retrying in 10 seconds...");
+    setTimeout(() => downloadData(retry++), 10000);
+  }
+  self.postMessage(downloadData());
   self.close();
-};
+}
