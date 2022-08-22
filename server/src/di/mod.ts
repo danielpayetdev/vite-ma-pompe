63372f93adf6848a7a10f23b823359821486e321
Daniel Payet <danieldu974@gmail.com>
2022-08-22 19:51:27 +0200

    remove server folder

--- server/src/di/mod.ts
+++
@@ -1,6 +0,0 @@
-/**
- * TEMPORAIRE: https://github.com/cmd-johnson/deno-reflect-metadata/issues/8
- */
-
-export { Bootstrapped, Injectable } from "./decorators.ts";
-export { bootstrap, Injector } from "./injector.ts";
