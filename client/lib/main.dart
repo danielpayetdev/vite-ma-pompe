e9d01b418494045c4d7bad2e5a9545ae61b5b10a
Daniel Payet <danieldu974@gmail.com>
2022-08-24 21:02:22 +0200

    init map

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:latlong2/latlong.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,15 +16,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -33,15 +26,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -49,67 +33,40 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  MapController controller = MapController(
-    initMapWithUserPosition: true,
-    initPosition: GeoPoint(latitude: 48.85292357696404, longitude: 2.3498870852732407),
-  );
-
-  @override
-  void initState() {
-    super.initState();
-    controller.enableTracking();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: OSMFlutter(
-        controller: controller,
-        trackMyPosition: false,
-        initZoom: 12,
-        minZoomLevel: 8,
-        maxZoomLevel: 14,
-        stepZoom: 1.0,
-        userLocationMarker: UserLocationMaker(
-          personMarker: const MarkerIcon(
-            icon: Icon(
-              Icons.location_history_rounded,
-              color: Colors.red,
-              size: 48,
-            ),
-          ),
-          directionArrowMarker: const MarkerIcon(
-            icon: Icon(
-              Icons.double_arrow,
-              size: 48,
-            ),
-          ),
-        ),
-        roadConfiguration: RoadConfiguration(
-          startIcon: const MarkerIcon(
-            icon: Icon(
-              Icons.person,
-              size: 64,
-              color: Colors.brown,
-            ),
-          ),
-          roadColor: Colors.yellowAccent,
-        ),
-        markerOption: MarkerOption(
-            defaultMarker: const MarkerIcon(
-          icon: Icon(
-            Icons.person_pin_circle,
-            color: Colors.blue,
-            size: 56,
-          ),
-        )),
-      ),
+      body: FutureBuilder<Position>(
+          future: Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.bestForNavigation),
+          builder: (context, snapshot) {
+            return snapshot.hasData
+                ? FlutterMap(
+                    options: MapOptions(
+                      center: LatLng(snapshot.data?.latitude ?? 0, snapshot.data?.longitude ?? 0),
+                      zoom: 16,
+                      maxBounds: LatLngBounds(
+                        LatLng(50.89, -2.00),
+                        LatLng(41.24, 9.68),
+                      ),
+                    ),
+                    layers: [
+                      TileLayerOptions(
+                        urlTemplate: "https://api.mapbox.com/styles/v1/danielpayet974/cl76it7vn003k14o0xg952ieu/tiles/512/{z}/{x}/{y}@2x?access_token={access_token}",
+                        additionalOptions: {"access_token": "pk.eyJ1IjoiZGFuaWVscGF5ZXQ5NzQiLCJhIjoiY2w3Nmlsem5iMDZhbjNwcGJma3FldnZ4aiJ9.ZK7lw66JWY__UzvpSX92GQ"},
+                      ),
+                      MarkerLayerOptions(
+                        markers: [
+                          Marker(
+                              point: LatLng(snapshot.data?.latitude ?? 0, snapshot.data?.longitude ?? 0),
+                              width: 80,
+                              height: 80,
+                              builder: (context) => const Icon(Icons.directions_car)),
+                        ],
+                      ),
+                    ],
+                  )
+                : const SizedBox();
+          }),
     );
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
   }
 }
